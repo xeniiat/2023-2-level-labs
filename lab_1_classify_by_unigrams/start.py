337ff150687642c1dd755774f447ec10d19f47a1
xeniia <ksu.torgasheva@mail.ru>
2023-10-02 23:18:07 +0300

    added command Fixed import style checks.

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 from lab_1_classify_by_unigrams.main import create_language_profile, detect_language
 
+
 def main() -> None:
     """
     Launches an implementation
