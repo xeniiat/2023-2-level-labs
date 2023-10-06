39cfdb92266fca0bd5c8df91a472c909752f8ece
xeniia <ksu.torgasheva@mail.ru>
2023-10-06 17:14:03 +0300

    added command again. Changes after seminar 5.0

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -31,6 +31,7 @@ def main() -> None:
     result = detect_language_advanced(unknown_profile, known_profiles)
     if result:
         return print_report(result)
+    return None
     assert result, "Detection result is None"
 
 
