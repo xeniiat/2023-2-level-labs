c0d8103ef75454bf49518aa5d850d6a468b5842a
xeniia <ksu.torgasheva@mail.ru>
2023-10-19 12:33:33 +0300

    added command again. Write a code

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -15,8 +15,8 @@ def main() -> None:
     result = None
     assert result, "Encoding is not working"
 
-    collect_frequencies(text, None, '</s>')
-
+    freq_dictionary = collect_frequencies(text, None, '</s>')
+    print(freq_dictionary)
 
 
 if __name__ == "__main__":
